4dbc09f3186d841e09e0611178abf7cf93bf50ab
TheVeryDarkness <3266343194@qq.com>
2025-05-21 17:49:58 +0800

    Add `toRef` for `Ref<T>`

--- src/components/ref.ts
+++ src/components/ref.ts
@@ -1,10 +1,24 @@
+import { ref, Ref, watch } from "vue";
+
 /**
  * @abstract An event target that dispatches an event when the value is set,
  * no matter whether the value is changed or not.
+ * Note that this not works in `watch` and `computed`, please use `toRef` instead.
+ * @template T The type of the value.
+ * @example
+ * const subscribedRef = new SubscribedRef(0);
+ * subscribedRef.subscribe(console.log);
+ * subscribedRef.value = 1; // logs 1
+ * subscribedRef.value = 1; // logs 1
+ * subscribedRef.value = 2; // logs 2
+ * subscribedRef.unsubscribe(console.log);
+ * subscribedRef.value = 3; // no logs
+ * subscribedRef.value = 3; // no logs
  */
 export class SubscribedRef<T> {
   private _value: T;
   private _subscribers: Set<(payload: T) => void> = new Set();
+  private _ref: Ref<T> | null = null;
   constructor(value: T) {
     this._value = value;
   }
@@ -14,6 +28,25 @@ export class SubscribedRef<T> {
   set value(value: T) {
     this._value = value;
     this.dispatchEvent(value);
+  }
+  /**
+   * @abstract Returns a ref that is updated when the value is set.
+   * @returns The ref.
+   */
+  toRef(): Ref<T> {
+    if (this._ref === null) {
+      // SAFETY: casted from `Ref<T | undefined>` to `Ref<T>`
+      // because we know that `this._ref` will be initialized later
+      this._ref = ref() as Ref<T>;
+      this._ref.value = this._value;
+      this.subscribe((new_value) => {
+        this._ref!.value = new_value;
+      });
+      watch(this._ref, (new_value) => {
+        this.value = new_value;
+      });
+    }
+    return this._ref;
   }
   private dispatchEvent(payload: T) {
     this._subscribers.forEach((subscriber) => subscriber(payload));
