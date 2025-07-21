d04c104978873b09a89e8f68a2498237f6c7de31
TheVeryDarkness <3266343194@qq.com>
2025-07-21 17:03:39 +0800

    Split demo data into several parts

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import Tree from "./components/Tree.vue";
 import { Data, Shape, TreeEvent } from "./components/types";
 import "./auto.css";
@@ -14,110 +14,157 @@ type T = {
   inChildrenShape?: (Shape | undefined)[];
   extensible?: boolean;
 };
-const data: T = {
-  name: "123123123123",
-  id: 123123123123,
-  outSelfShape: "diamond",
-  dashArray: "4 2",
-  children: [
+function lazy(_: T): T[] {
+  return [
     {
-      name: "445",
-      id: 445,
-      color: "red",
+      name: new Date().toString(),
+      outSelfShape: "diamond",
+      dashArray: "4 2",
+      children: lazy,
+    },
+    {
+      name: new Date().toString(),
       outSelfShape: "circle",
-      outSelfFill: "red",
-      dashArray: 4,
-      children: [
-        { name: "123132", color: "cyan", children: [] },
-        { name: "xdfscds", color: "cyan", children: [] },
-      ],
-      inChildrenShape: ["arrow", "diamond"],
+      dashArray: "2 4",
+      children: lazy,
     },
-    {
-      name: "445",
-      id: "445",
-      color: "green",
-      outSelfShape: "triangle",
-      dashArray: "2",
-      children: [{ name: "9999999", color: "cyan", children: [] }],
-      inChildrenShape: ["circle"],
+  ];
+}
+const datas: T[] = [
+  {
+    name: "123123123123",
+    id: 123123123123,
+    outSelfShape: "diamond",
+    dashArray: "4 2",
+    children: [
+      {
+        name: "445",
+        id: 445,
+        color: "red",
+        outSelfShape: "circle",
+        outSelfFill: "red",
+        dashArray: 4,
+        children: [
+          { name: "123132", color: "cyan", children: [] },
+          { name: "xdfscds", color: "cyan", children: [] },
+        ],
+        inChildrenShape: ["arrow", "diamond"],
+      },
+      {
+        name: "445",
+        id: "445",
+        color: "green",
+        outSelfShape: "triangle",
+        dashArray: "2",
+        children: [{ name: "9999999", color: "cyan", children: [] }],
+        inChildrenShape: ["circle"],
+      },
+      { name: "7890", color: "red", children: [], extensible: true },
+      { name: "'", color: "green", children: [], extensible: true },
+      { name: "?", color: "green", children: () => [{ name: new Date().toString(), color: "blue", children: [] }], extensible: true },
+      { name: "~~~~", color: "green", outSelfShape: "arrow", children: [{ name: "????", color: "blue", children: [] }], extensible: true },
+      { name: "~~~~~~~~~~~~~~~~~~~~~~~~~", color: "green", children: [{ name: "????????", color: "blue", children: [] }], extensible: true },
+      {
+        name: "abcdefgdwewok",
+        id: "kkkk",
+        color: "red",
+        children: [{ name: "xdncsc", color: "green", children: [] }],
+        inChildrenShape: ["triangle"],
+      },
+      {
+        name: "abcdefghijklmnopqrstuvwxyz",
+        color: "red",
+        extensible: true,
+        children: [{ name: "abcdefghijklmnopqrstuvwxyz", color: "green", children: [] }],
+      },
+      {
+        name: "abcdefghijklmnopqrstuvwxyz",
+        color: "red",
+        children: [
+          { name: "a", color: "green", children: [] },
+          { name: "abcdefghijklmnopqrstuvwxyz", color: "green", children: [] },
+        ],
+      },
+      {
+        name: "328948923",
+        color: "red",
+        extensible: true,
+        children: [],
+      },
+      {
+        name: "abcdefgdwewok",
+        color: "red",
+        extensible: true,
+        children: [
+          { name: "——", color: "green", children: [] },
+          { name: "x", color: "green", children: [] },
+          { name: "_", color: "green", children: [] },
+          { name: "y", color: "green", children: [] },
+        ],
+        inChildrenShape: ["arrow", "circle", "diamond", "triangle"],
+      },
+      {
+        name: "abcdefgdwewoknjnjonomiodjewidjwoedeowno",
+        color: "red",
+        extensible: true,
+        children: [
+          { name: "——", color: "green", children: [] },
+          { name: "x", color: "green", children: [] },
+          { name: "_", color: "green", children: [] },
+          { name: "y", color: "green", children: [] },
+        ],
+      },
+    ],
+  },
+  {
+    name: "shapes",
+    id: 1,
+    outSelfShape: "diamond",
+    dashArray: "4 2",
+    children: [
+      {
+        name: "0",
+        children: [
+          { name: "a", extensible: true, children: [], outSelfShape: "arrow", outSelfFill: "currentColor" },
+          { name: "b", extensible: true, children: [], outSelfShape: "circle", outSelfFill: "currentColor" },
+          { name: "c", extensible: true, children: [], outSelfShape: "diamond", outSelfFill: "currentColor" },
+          { name: "d", extensible: true, children: [], outSelfShape: "triangle", outSelfFill: "currentColor" },
+        ],
+      },
+      {
+        name: "1",
+        children: [
+          { name: "a", extensible: true, children: [], outSelfShape: "arrow", outSelfFill: "blue" },
+          { name: "b", extensible: true, children: [], outSelfShape: "circle", outSelfFill: "blue" },
+          { name: "c", extensible: true, children: [], outSelfShape: "diamond", outSelfFill: "blue" },
+          { name: "d", extensible: true, children: [], outSelfShape: "triangle", outSelfFill: "blue" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "lazy",
+    id: 1,
+    outSelfShape: "diamond",
+    dashArray: "4 2",
+    children() {
+      return [
+        {
+          name: new Date().toString(),
+          id: 1,
+          outSelfShape: "diamond",
+          dashArray: "4 2",
+          children: lazy,
+        },
+      ];
     },
-    { name: "7890", color: "red", children: [], extensible: true },
-    { name: "'", color: "green", children: [], extensible: true },
-    { name: "?", color: "green", children: () => [{ name: new Date().toString(), color: "blue", children: [] }], extensible: true },
-    { name: "~~~~", color: "green", outSelfShape: "arrow", children: [{ name: "????", color: "blue", children: [] }], extensible: true },
-    { name: "~~~~~~~~~~~~~~~~~~~~~~~~~", color: "green", children: [{ name: "????????", color: "blue", children: [] }], extensible: true },
-    {
-      name: "abcdefgdwewok",
-      id: "kkkk",
-      color: "red",
-      children: [{ name: "xdncsc", color: "green", children: [] }],
-      inChildrenShape: ["triangle"],
-    },
-    {
-      name: "abcdefghijklmnopqrstuvwxyz",
-      color: "red",
-      extensible: true,
-      children: [{ name: "abcdefghijklmnopqrstuvwxyz", color: "green", children: [] }],
-    },
-    {
-      name: "abcdefghijklmnopqrstuvwxyz",
-      color: "red",
-      children: [
-        { name: "a", color: "green", children: [] },
-        { name: "abcdefghijklmnopqrstuvwxyz", color: "green", children: [] },
-      ],
-    },
-    {
-      name: "328948923",
-      color: "red",
-      extensible: true,
-      children: [],
-    },
-    {
-      name: "abcdefgdwewok",
-      color: "red",
-      extensible: true,
-      children: [
-        { name: "——", color: "green", children: [] },
-        { name: "x", color: "green", children: [] },
-        { name: "_", color: "green", children: [] },
-        { name: "y", color: "green", children: [] },
-      ],
-      inChildrenShape: ["arrow", "circle", "diamond", "triangle"],
-    },
-    {
-      name: "abcdefgdwewoknjnjonomiodjewidjwoedeowno",
-      color: "red",
-      extensible: true,
-      children: [
-        { name: "——", color: "green", children: [] },
-        { name: "x", color: "green", children: [] },
-        { name: "_", color: "green", children: [] },
-        { name: "y", color: "green", children: [] },
-      ],
-    },
-    {
-      name: "0",
-      children: [
-        { name: "a", extensible: true, children: [], outSelfShape: "arrow", outSelfFill: "currentColor" },
-        { name: "b", extensible: true, children: [], outSelfShape: "circle", outSelfFill: "currentColor" },
-        { name: "c", extensible: true, children: [], outSelfShape: "diamond", outSelfFill: "currentColor" },
-        { name: "d", extensible: true, children: [], outSelfShape: "triangle", outSelfFill: "currentColor" },
-      ],
-    },
-    {
-      name: "1",
-      children: [
-        { name: "a", extensible: true, children: [], outSelfShape: "arrow", outSelfFill: "blue" },
-        { name: "b", extensible: true, children: [], outSelfShape: "circle", outSelfFill: "blue" },
-        { name: "c", extensible: true, children: [], outSelfShape: "diamond", outSelfFill: "blue" },
-        { name: "d", extensible: true, children: [], outSelfShape: "triangle", outSelfFill: "blue" },
-      ],
-    },
-  ],
-};
-const _data: Data<T, "id"> = data;
+  },
+];
+// Ensure data conforms to Data interface with 'id' as key
+const _data = computed(function (): Data<T, "id"> {
+  return datas[treeData.value];
+});
+const treeData = ref(0);
 let tree = ref();
 let state = {
   active: ref<string | number | undefined>(undefined),
@@ -150,9 +197,16 @@ window.addEventListener("click", () => {
   <p>Shift + Left Click = Switch direction</p>
   <p>Ctrl + Left Click = Scroll into view</p>
   <p>Right Click = Collapse or expand</p>
-  <label for="active-node">Active:</label>
+  <label for="tree-data">Tree Data: </label>
+  <select id="tree-data" title="Tree Data" v-model="treeData">
+    <option value="0">Arbitrary</option>
+    <option value="1">Shapes</option>
+    <option value="2">Lazy Evaluation</option>
+  </select>
+  <br />
+  <label for="active-node">Active: </label>
   <input type="text" id="active-node" title="Active Node" v-model="state.active.value" />
-  <label for="hover-node">Hover:</label>
+  <label for="hover-node">Hover: </label>
   <input type="text" id="hover-node" title="Hover Node" v-model="state.hover.value" />
   <br />
   <div class="container">
@@ -160,7 +214,7 @@ window.addEventListener("click", () => {
   </div>
   <br />
   <div class="container">
-    <textarea id="tree-data" title="Tree Data">{{ JSON.stringify(data, null, 2) }}</textarea>
+    <textarea id="tree-data" title="Tree Data">{{ JSON.stringify(datas[treeData], null, 2) }}</textarea>
   </div>
 </template>
 
